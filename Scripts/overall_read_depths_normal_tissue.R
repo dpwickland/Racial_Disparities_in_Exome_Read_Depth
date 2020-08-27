e813baa1ec465f324f73e40c54d53c236be2dfda
dpwickland <wicklan2@illinois.edu>
2020-08-27 15:32:02 -0500

    removed row names, quotation marks

--- Scripts/overall_read_depths_normal_tissue.R
+++ Scripts/overall_read_depths_normal_tissue.R
@@ -168,4 +168,4 @@ setTimeLimit(cpu = Inf, elapsed = Inf, transient = FALSE)
   combined_data$Cancer <- CANCER_NAME
 
 #save dataset
-write.table(x=combined_data,file=paste0('/home/mayo/m187735/s212975.Wickland_Immunomics/processing/TCGA/processed_data/overall_depths/',CANCER_NAME,'_overall_depths_etc_NORMAL.txt'),sep='\t')
+write.table(x=combined_data,file=paste0('/home/mayo/m187735/s212975.Wickland_Immunomics/processing/TCGA/processed_data/overall_depths/',CANCER_NAME,'_overall_depths_etc_NORMAL.txt'),sep='\t',row.names=FALSE,quote=FALSE)
